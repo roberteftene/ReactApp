4ed5aaf982a75653fb953e1b24d58a1b3316173a
roberteftene <roberteftene25@gmail.com>
2020-04-20 23:18:18 +0300

    Done first day tasks

---
+++ my-app/src/components/widget.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+interface Props {
+}
+interface State {
+}
+
+class Widget extends Component < Props, State > {
+    state = {}
+
+    render() {
+        return (
+            <div>
+                Widget
+            </div>
+        )
+    }
+}
+
+export default Widget
